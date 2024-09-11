8dc9491169acee164bc9839d120d35c9dc267552
majumxrs <mariaescola06@gmail.com>
2024-09-11 09:19:04 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComuniQBD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
