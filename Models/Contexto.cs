156d54b73e9f9e081b38959366975d01d1137a09
majumxrs <mariaescola06@gmail.com>
2024-09-11 09:49:52 -0300

    Program formada e contexto feito

--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -1,6 +1,16 @@
-﻿namespace ComuniQBD.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ComuniQBD.Models
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+        //public DbSet<Produto> Produto { get; set; }
+        //public DbSet<TipoProduto> TipoProduto { get; set; }
+        //public DbSet<EntradaeSaida> EntradaeSaida { get; set; }
+        //public DbSet<TipoMovimentacao> TipoMovimentacao { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
+using ComuniQBD.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+/*builder.Services.AddDbContext<Contexto> //Gabi
+    (options => options.UseSqlServer("Data Source=SP-1491023\\SQLSENAI;Initial Catalog = ComuniQBD;Integrated Security = True;TrustServerCertificate = True"));*/
+
+/*builder.Services.AddDbContext<Contexto> //Edu
+    (options => options.UseSqlServer("Data Source=SP-1491021\\SQLSENAI;Initial Catalog = ComuniQBD;Integrated Security = True;TrustServerCertificate = True"));*/
+
+builder.Services.AddDbContext<Contexto> //Maju
+    (options => options.UseSqlServer("Data Source=SP-1491028\\SQLSENAI;Initial Catalog = ComuniQBD;Integrated Security = True;TrustServerCertificate = True"));
+
+/*builder.Services.AddDbContext<Contexto> //Rafa
+    (options => options.UseSqlServer("Data Source=SP-1491022\\SQLSENAI;Initial Catalog = ComuniQBD;Integrated Security = True;TrustServerCertificate = True"));*/
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
