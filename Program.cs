79e7e5d78e213f25f409d4b9dbcb205bb844ed6f
majumxrs <mariaescola06@gmail.com>
2024-10-29 10:47:14 -0300

    merch

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<Contexto> //Gabi
-  (options => options.UseSqlServer("Data Source=SP-1491023\\SQLSENAI;Initial Catalog = ComuniQBD;Integrated Security = True;TrustServerCertificate = True"));
+//builder.Services.AddDbContext<Contexto> //Gabi
+//  (options => options.UseSqlServer("Data Source=SP-1491023\\SQLSENAI;Initial Catalog = ComuniQBD;Integrated Security = True;TrustServerCertificate = True"));
 
 //builder.Services.AddDbContext<Contexto> //Edu
   // (options => options.UseSqlServer("Data Source=SP-1491021\\SQLSENAI;Initial Catalog = ComuniQBD;Integrated Security = True;TrustServerCertificate = True"));
 
 //builder.Services.AddDbContext<Contexto> //Maju
-//   (options => options.UseSqlServer("Data Source=SP-1491028\\SQLSENAI;Initial Catalog = ComuniQBD;Integrated Security = True;TrustServerCertificate = True"));
+ //  (options => options.UseSqlServer("Data Source=SP-1491028\\SQLSENAI;Initial Catalog = ComuniQBD;Integrated Security = True;TrustServerCertificate = True"));
 
 /*builder.Services.AddDbContext<Contexto> //Rafa
     (options => options.UseSqlServer("Data Source=SP-1491022\\SQLSENAI;Initial Catalog = ComuniQBD;Integrated Security = True;TrustServerCertificate = True"));*/
